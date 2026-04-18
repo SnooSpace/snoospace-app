7af8d9d46fce325a96da3084cf549e5e885f170b
harshiths440 <harshiths440@gmail.com>
2026-04-19 00:56:21 +0530

    Removed duplicate root

--- components/discover/index.js
+++
@@ -1,4 +0,0 @@
-// Discover Components V2 - Category-based event discovery
-export { default as DiscoverFeedV2 } from "./DiscoverFeedV2";
-export { default as CategoryCarousel } from "./CategoryCarousel";
-export { default as DiscoverEventCard } from "./DiscoverEventCard";
